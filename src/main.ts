601b04ad634eb881369e3a51def0764138285d66
Sebastien PENCREACH <pencreach.sebastien@gmail.com>
2022-10-06 21:28:57 +0200

    chore: add user plugin in the app

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+import { userPlugin } from "@/proposals/userPlugin";
+import { InMemoryUsers } from "@/infrastructure/inMemoryUsers";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(userPlugin(InMemoryUsers())); //TODO: replace the in memory implementation
 
 app.mount("#app");
