991939f13c3d22ecc05ba957701702c0c9c5f6aa
Sebastien PENCREACH <pencreach.sebastien@gmail.com>
2022-10-24 20:15:53 +0000

    fix: maps from supabase

--- src/infrastructure/supabaseProposals.ts
+++ src/infrastructure/supabaseProposals.ts
@@ -7,18 +7,30 @@ import type {
 import { calendars } from "@/infrastructure/calendars";
 import { createClient } from "@supabase/supabase-js";
 import { createProposal } from "@/proposals/domain/proposal";
+import dayjs from "dayjs";
+import type { definitions } from "./types/supabase";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
+type SupabaseProposal = definitions["proposals"];
+
 const loadProposals = async (): Promise<ProposalCollection> => {
   const { data, error } = await supabase
     .from("proposals")
     .select()
     .gte("date", calendars.today().date);
-  return data as ProposalCollection;
+  const supaProposal = data as SupabaseProposal[];
+  return supaProposal.map<Proposal>((supaProposal) => {
+    return {
+      id: supaProposal.id,
+      date: dayjs(supaProposal.date).toISOString(),
+      slot: supaProposal.slot,
+      user: supaProposal.user,
+    } as Proposal;
+  });
 };
 
 const addProposal = async (proposal: Proposal) => {
