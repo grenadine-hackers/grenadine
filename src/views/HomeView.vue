30c6e1a177d178a7795cb80da8c6c4cdbb9209f8
Sebastien PENCREACH <pencreach.sebastien@gmail.com>
2022-10-24 21:11:30 +0000

    feat: add some colors

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@ import MyProposals from "@/proposals/MyProposals.vue";
 </script>
 
 <template>
-  <h1>Grenadine</h1>
+  <h1><img src="grenadine.svg" height="48" width="48" /> Grenadine</h1>
   <NearProposals :slot-type="SlotType.LUNCH" />
   <NearProposals :slot-type="SlotType.DINNER" />
   <MyProposals />
