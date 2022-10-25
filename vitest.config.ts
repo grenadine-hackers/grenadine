42bea3ba0aa6372eabcdede4717d0cd164045ab8
Sebastien PENCREACH <pencreach.sebastien@gmail.com>
2022-10-25 14:37:03 +0000

    chore: removes unused jsx support from vite

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { URL, fileURLToPath } from "url";
 
 import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
